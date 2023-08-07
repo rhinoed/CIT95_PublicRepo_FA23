a85234dcc88e203ad1c139d746cc5f6e07b10bba
Mark Edmunds <edmundsmd1@gmail.com>
2023-08-06 23:59:30 -0700

    adding hello world.py

---
+++ Giraffe Academy/scripts/helloWorld.py
@@ -0,0 +1,7 @@
+# hello world script by Mark Edmunds for BIT 95
+username = input("Please enter your name:")
+
+if username == "":
+    print("hello world")
+else:
+    print("hello " + username + " isn't this such beautiful world we live in?")
